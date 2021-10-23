94878dfde44a06d1d3ef789ab802aeaa6b51c669
David Iglesias <davidiglesanchez@gmail.com>
2021-10-23 21:04:02 +0200

    Default export. Version 0.0.2

--- src/Logger.ts
+++ src/Logger.ts
@@ -2,9 +2,9 @@ import { Log } from "./Log";
 import { LogImpl } from "./LogImpl";
 
 export class Logger {
-
   static getLogger(className?: string): Log {
     return new LogImpl(className);
   }
-  
 }
+
+export default Logger;
